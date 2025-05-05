90e5086f6c1f7eab8c8da20969d8fb9489a1932d
Spencer Arney <156545579+ttu-sarney@users.noreply.github.com>
2025-05-04 23:21:19 -0500

    god

--- backend/routes.js
+++ backend/routes.js
@@ -13,15 +13,62 @@ app.use(cors())
 app.use(express.json())
 
 app.post('/classes', (req, res, next) => {
-    res.status(200).json({message:"I am alive"})
+   
+    let strClassID = uuidv4()
+    let datStartDate = req.body.startDate
+    let datEndDate = req.body.endDate
+    let strClassname = req.body.name
+    let strClassDescription = req.body.description
+    let strUserID = req.body.id
+    let boolIsAdmin = req.body.isAdmin
+    let boolIsActive = req.body.active
+    let strcommand1 = `INSERT INTO tblClasses VALUES (?,?,?,?,?,?)`
+    db.run(strcommand1, [strClassID,strClassname,strClassDescription,datStartDate,datEndDate , boolIsActive], function (err) {
+        if(err){
+            console.log(err)
+            res.status(400).json({
+                status:"error",
+                message:err.message
+            })
+        } else {
+           let strStuClassID = uuidv4()
+           let strcommand2 = `INSERT INTO tblStuClass VALUES (?,?,?,?)`
+           db.run(strcommand2, [strStuClassID,strUserID,strClassID,boolIsAdmin], function (err) {
+            if(err){
+                console.log(err)
+                res.status(400).json({
+                    status:"error",
+                    message:err.message
+                })
+            } else {
+                res.status(201).json({
+                    status:"success"
+                })
+            }
+           })
 
+        }
+})
 })
 
 app.put('/classes', (req, res, next) => {
     
 })
-app.get('/classes', (req, res, next) => {
+
+app.get('/classes/instructorClasses', (req, res, next) => {
     
+    let strUserID = req.body.id
+    let boolIsAdmin = req.body.isAdmin
+    let sql = `SELECT ClassName, Description,StartDate,EndDate,IsActive FROM tblClasses JOIN tblStuClass ON tblClasses.ClassID = tblStuClass.ClassID WHERE tblStuClass.UserID = ? AND tblStuClass.Instructor = ?;`
+
+    db.all(sql, [strUserID, boolIsAdmin], (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ status: "error", message: err.message });
+        } else {
+            res.json(rows);
+        }
+    })
 })
 
 app.get('/',(req,res,next) => {
